89f9d60cb1160d664af562956326efe081b6e6a6
Rahul Pon <theflyingrahul@gmail.com>
2020-04-30 01:47:44 +0530

    Added AboutPage.

--- lib/screens/home/about/AboutFragment.dart
+++ lib/screens/home/about/AboutFragment.dart
@@ -8,14 +8,181 @@ class AboutFragment extends StatelessWidget {
           child: Padding(
         padding: const EdgeInsets.all(20.0),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Text(
-              "AppName",
-              style: TextStyle(
-                fontFamily: "Poppins",
+            Row(
+              children: <Widget>[
+                Image.asset(
+                  "images/xstack.png",
+                  fit: BoxFit.fill,
+                  width: 75.0,
+                  height: 75.0,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(right: 10),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      "Communicator",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 23,
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Text(
+                      "for xStack",
+                      style: TextStyle(
+                        color: Colors.deepPurple,
+                        fontSize: 20,
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 20),
+            ),
+            RichText(
+              text: TextSpan(
+                text:
+                'xStack is an Enterprise Resource Planning software designed, built and maintained by ',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontFamily: 'Poppins',
+                  fontWeight: FontWeight.w600,
+                ),
+                children: <TextSpan>[
+                  TextSpan(
+                    text: 'Pon Rahul',
+                    style: TextStyle(
+                      color: Colors.deepPurple,
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  TextSpan(
+                    text: ' and ',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  TextSpan(
+                    text: 'Daniel Mark',
+                    style: TextStyle(
+                      color: Colors.deepPurple,
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  TextSpan(
+                    text: '. It was made possible by the closed-source Mer project and the MixSpace project.',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ],
               ),
             ),
-            Text("Communicator"),
+            Padding(
+              padding: EdgeInsets.only(top: 20),
+            ),
+            Flexible(
+              child: Text(
+                "xStack Client: mer Mobile for Android",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontFamily: 'Poppins',
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+            Wrap(
+              children: <Widget>[
+                Text(
+                  "© 2020 ",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                Text(
+                  "Mer Community",
+                  style: TextStyle(
+                    color: Colors.deepPurple,
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                Text(
+                  " | ",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                Text(
+                  "MixSpace Internet Services",
+                  style: TextStyle(
+                    color: Colors.deepPurple,
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 20),
+            ),
+            Flexible(
+              child: Text(
+                "Terms of use",
+                style: TextStyle(
+                  color: Colors.deepPurple,
+                  fontFamily: 'Poppins',
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 20),
+            ),
+            Flexible(
+              child: Text(
+                "Privacy statement",
+                style: TextStyle(
+                  color: Colors.deepPurple,
+                  fontFamily: 'Poppins',
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 20),
+            ),
+            Flexible(
+              child: Text(
+                "xStack services agreement",
+                style: TextStyle(
+                  color: Colors.deepPurple,
+                  fontFamily: 'Poppins',
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ),
           ],
         ),
       )),
