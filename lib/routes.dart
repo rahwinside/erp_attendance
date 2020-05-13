9ab8292d3004664c8591044eaccd01803ec7433b
Rahul Pon <theflyingrahul@gmail.com>
2020-05-14 02:27:52 +0530

    Login logic has been tested.

--- lib/routes.dart
+++ lib/routes.dart
@@ -3,13 +3,11 @@ import 'package:attendance/screens/home/home_page.dart';
 import 'package:attendance/screens/login/login_screen.dart';
 import 'package:flutter/material.dart';
 
-import 'custom_widgets/clickable.dart';
-
 final routes = {
   '/login': (BuildContext context) => new LoginScreen(),
-  '/': (BuildContext context) => new NameClickable(),
+//  '/': (BuildContext context) => new NameClickable(),
   '/home': (BuildContext context) => new HomeScreen(),
   '/changePassword': (BuildContext context) => new ChangePasswordScreen(),
-//  '/': (BuildContext context) => new LoginScreen(),
+  '/': (BuildContext context) => new LoginScreen(),
 
 };
